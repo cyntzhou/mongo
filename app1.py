a3b933597945b2a6741c5765e4e48562ade3f571
Justin Kim <yulli@yulli.org>
2014-10-30 12:06:09 -0400

    ¯\_(ツ)_/¯

--- app1.py
+++ app1.py
@@ -5,24 +5,24 @@ import db
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    if "username" in session:
-        return "Logged in as %s" % (session["username"])
+    if 'username' in session:
+        return 'Logged in as %s' % (session['username'])
     return render_template('home.html')
 
 
-@app.route("/login", methods=["GET","POST"])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method=="GET":
-        return render_template("login.html")
+    if request.method == 'GET':
+        return render_template('login.html')
 
-    button = request.form["button"]
-    username = request.form["username"]
-    password = request.form["password"]
+    button = request.form['button']
+    username = request.form['username']
+    password = request.form['password']
     valid_user = valid(username,password)
-    if button=="cancel" or not(valid_user):
-        return render_template("login.html")
+    if button == 'cancel' or not(valid_user):
+        return render_template('login.html')
     else:
         criteria = {'username': username, 'password': password}
         user = db.find_user(criteria)
@@ -30,24 +30,24 @@ def login():
             session['username'] = username
             return redirect('/')
         else:
-            return "Invalid username and password combination"
+            return 'Invalid username and password combination'
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route('/register', methods=['GET','POST'])
 def register():
-    if request.method=="GET":
-        return render_template("register.html")
+    if request.method=='GET':
+        return render_template('register.html')
 
-    button = request.form["button"]
-    username = request.form["username"]
-    password = request.form["password"]
+    button = request.form['button']
+    username = request.form['username']
+    password = request.form['password']
     valid_user = valid(username, password)
-    if button=="cancel":
-        return redirect("/")
+    if button == 'cancel':
+        return redirect('/')
     else:
         criteria = {'username': username}
         if db.find_user(criteria):
-            return render_template("register.html")
+            return render_template('register.html')
         else:
             user_params ={'username': username, 'password':password}
             db.new_user(user_params)
@@ -55,17 +55,17 @@ def register():
             return redirect('/')
 
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
-    session.pop("username", None)
-    return redirect("/login")
+    session.pop('username', None)
+    return redirect('/login')
 
 
 def valid(username, password):
     return True
 
 
-if __name__=="__main__":
-    app.secret_key="Happy Halloween"
+if __name__ == '__main__':
+    app.secret_key = 'Happy Halloween'
     app.debug=True
     app.run();
